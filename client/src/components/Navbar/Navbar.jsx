cf33cc7b078c0e154069cf557e61220e4cd8eeb0
Eshwar <eshwarvengala30@gmail.com>
2025-03-18 15:12:34 +0530

    basic routing and files

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
+import "./Navbar.css"
 function Navbar() {
   return (
       <div className="navbar">
-      nav bar11
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/news">News</NavLink>
+        <NavLink to="/simulators">Simulators</NavLink>
+        <NavLink to="/books">Books</NavLink>
+        <NavLink to="/career">Career</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
       </div>
   )
 }
