7eb04f4d6cd21a5276dfeb4fd14a72bce65510b5
Eshwar <eshwarvengala30@gmail.com>
2025-03-26 15:32:03 +0530

    resolve issue

--- client/src/Context/UserDetails.jsx
+++ client/src/Context/UserDetails.jsx
@@ -4,10 +4,7 @@ export const UserDetailsContext = createContext();
 
 export const UserDetailsProvider = ({ children }) => {
 
-  const [user,setUser] = useState({
-    "username":"ankith",
-    "eamil":"ankithkumar9618@gmail.com"
-  });
+  const [user,setUser] = useState(null);
   const [userDetails,setUserDetails] = useState(null);
   
   return (
